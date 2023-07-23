367e445bf9136b81c842c3ad8205cafba63b1cdf
Игорь Горбенко <gorbenkoigorek@mail.ru>
2023-07-23 22:33:21 +0300

    added settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -104,14 +104,11 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'collected_static'
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
